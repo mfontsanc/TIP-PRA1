7e809db62bc580da59fd78a04aba84bf2027b5cf
ibarrerabe <92327889+ibarrerabe@users.noreply.github.com>
2021-10-21 23:32:49 +0200

    Update webScraper.py
    La última versió!!!!!

--- src/webScraper.py
+++ src/webScraper.py
@@ -19,12 +19,12 @@ class WebScraper:
             (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
         }
 
-    def __download_web(self):
+    def __download_web(self, url):
         """
         Descarrega la web guardada a la variable self.url
         :return: Objecte del tipus BeautifulSoup (contingut de la web en HTML)
         """
-        page = requests.get(self.url, self.headers)
+        page = requests.get(url)
 
         if page.status_code == 200:
             return BeautifulSoup(page.content, "html.parser")
@@ -35,7 +35,8 @@ class WebScraper:
         :return: null
         """
         list_categories = self.__get_categories(content)
-        print(list_categories)
+
+        return list_categories
 
     def __get_categories(self, content):
         """
@@ -56,10 +57,53 @@ class WebScraper:
 
         return list_categories
 
+    def __get_articles(self, content):
+        """
+        Donat la URL d'una categoria,
+        obté el llistat d`'URL d'accés dels articles
+        :param content: Objecte del tipus String
+        :return: list_: Array
+        """
+        content_html = self.__download_web(content)
+        articles = content_html.findChildren("article", recursive=True)
+        list_articles = []
+        for articles_a in articles:
+            href_ = articles_a.find('a')
+            if href_:
+                url_art = href_.get('href')
+                if url_art:
+                    list_articles.append(url_art)
+
+        return list_articles
+
+    def __get_detalls (self, content):
+         """
+         Donat la URL d'un article,
+         obté la informació de l'article que volem
+         guardar en el csv.
+         :return: list_: Array
+         """
+         content_html = self.__download_web(content[0])
+         categories = content_html.find("script", {"type": "application/ld+json"})
+         detalls = categories.find("@type")
+         if content[1] == "Què Fer":
+            print(content[1])
+
+
+    def __init_csv(self, content):
+        for content_a in content:
+            articles = self.__get_articles(content_a[1])
+            for articles_a in articles:
+                   if "//www.timeout.cat/" not in articles_a:
+                       articles_a = self.url + articles_a
+
+                   self.__get_detalls([articles_a, content[1]])
+
     def init_process(self):
         """
         Executa el procés de web scraping
         :return: null
         """
-        content_html = self.__download_web()
-        self.__get_links(content_html)
+        content_html = self.__download_web(self.url)
+        links_url = self.__get_links(content_html)
+        self.__init_csv(links_url)
