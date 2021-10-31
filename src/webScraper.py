ce6617a4717545c3f8f4c692b7306969a6055128
Maria Font <mfontsanc@uoc.edu>
2021-10-31 20:09:33 +0100

    Guardar al CSV

--- src/webScraper.py
+++ src/webScraper.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 from bs4 import BeautifulSoup
 
 
@@ -7,6 +8,7 @@ class WebScraper:
     def __init__(self, url):
         self.url = url
         self.output_data = []
+        self.output_file = "../csv/ArticlesTimeOutCat.csv"
         self.headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,\
             */*;q=0.8",
@@ -26,12 +28,11 @@ class WebScraper:
         :return: Objecte del tipus BeautifulSoup (contingut de la web en HTML)
         """
         try:
-            print(url)
             page = requests.get(url)
 
             if page.status_code == 200:
                 return BeautifulSoup(page.content, "html.parser")
-        finally:
+        except:
             print("Error getting the URL, the process continues: " + url)
 
     def get_links(self, content):
@@ -57,7 +58,7 @@ class WebScraper:
             href = self.url + category_a.get('href')
             name = category_a.get('title')
 
-            if href and name:
+            if href and name and name != 'Separator':
                 list_categories.append([name, href])
 
         return list_categories
@@ -85,54 +86,76 @@ class WebScraper:
 
             return list_articles
 
-    def get_details(self, content):
+    def get_details(self, url, category):
         """
         Donat la URL d'un article, obté la informació de l'article que volem
         guardar en el csv.
-        :param content:
-        :return: list_: Array
+        :param category: category on s'ha trobat l'article i la URL.
+        :param url: URL amb els detalls de l'article.
+        :return: None
         """
-        content_html = self.download_web(content[0])
+        content_html = self.download_web(url)
 
         if content_html:
             data_str = content_html.find("script", {"type": "application/ld+json"})
 
             if data_str:
                 data_json = json.loads(data_str.contents[0])
-
                 if data_json['@type'] == 'Article':
-                    return {
+                    self.output_data.append({
+                        "categoria": category[0],
+                        "urlCategoria": category[1],
+                        "article": data_json['name'],
                         "descripcio": data_json['description'],
                         "tipus": data_json['@type'],
-                        "url": content[0],
+                        "urlArticle": url,
+                        "creador": data_json['creator'],
                         "dataPublicacio": data_json['datePublished'],
                         "paraulesClaus": data_json['keywords']
-                    }
+                    })
+
+    def save_csv(self):
+        """
+        Guarda les dades obtingudes en un CSV.
+        :return: None
+        """
+        header = ['categoria', 'urlCategoria', 'article', 'descripcio', 'tipus', 'urlArticle', 'creador',
+                  'dataPublicacio', 'paraulesClaus']
+
+        file = open(self.output_file, 'w', newline='', encoding="utf-8-sig")
+        writer = csv.writer(file, delimiter=";")
+        # Guarda els headers
+        writer.writerow(header)
+        # Guarda la informació de la variable output_data
+        for data in self.output_data:
+            writer.writerow(list(data.values()))
+
+        file.close()
 
     def init_csv(self, content):
-        all_data = []
         for content_a in content:
+            print("Getting articles with the category: " + content_a[0])
             articles = self.get_articles(content_a[1])
             for articles_a in articles:
                 if "//www.timeout.cat/" not in articles_a:
                     articles_a = self.url + articles_a
+                self.get_details(articles_a, content_a)
 
-                data = self.get_details([articles_a, content[1]])
-                print(data)
-                if data is not None:
-                    all_data.append(data)
-
-        print(all_data)
+        print("Saving data into the dataset.")
+        self.save_csv()
 
     def init_process(self):
         """
         Executa el procés de web scraping
         :return: null
         """
+        print("Starting process...")
         content_html = self.download_web(self.url)
 
         if content_html:
             links_url = self.get_links(content_html)
+            print("Total number of categories: ", len(links_url))
             self.init_csv(links_url)
+            print("Dataset has been created: " + self.output_file)
         else:
             print("Web scraping process ended without results!")
